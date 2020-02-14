a790d872a0cdb96c189c727c53ed2b3e45d9b839
Druhin Tarafder <contact@druh.in>
2020-02-14 21:44:40 +0530

    Update lorem.js

--- lorem.js
+++ lorem.js
@@ -1,1 +1,37 @@
 
+var a = document.getElementById("lorem").innerHTML;
+var ipsum = parseInt(a, 10);
+
+//element.innerHTML
+
+
+
+
+function random(items) {
+	return items[Math.floor(Math.random() * items.length)];
+}
+
+
+var i,j;
+var word = "",
+	sentence = "";
+var h1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+//no. of words
+for (i = 1; i <= ipsum; i++) {
+
+	//no. of letters
+	var l = random(n);
+
+	for (j = 1; j <= l; j++) {
+		var letter = random(h1);
+		word = word + letter;
+	}
+	word = "";
+	sentence = sentence + word;
+
+}
+
+console.log(sentence);
