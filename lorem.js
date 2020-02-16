741c0b29f5702b9a8a2b51bbdc551c345a55fa86
Druhin Tarafder <druhinpro@gmail.com>
2020-02-16 11:45:19 +0530

    Add files via upload

--- lorem.js
+++ lorem.js
@@ -1,4 +1,4 @@
-var a = document.getElementById("lorem").innerHTML;
+/*var a = document.getElementById("lorem").innerHTML;
 var ipsum = parseInt(a, 10);
 
 function random(items) {
@@ -26,5 +26,48 @@ for (i = 1; i <= ipsum; i++) {
 	sentence = sentence + " " + word;
 	word = "";
 }
-console.log(sentence);
-document.getElementById("lorem").innerHTML = sentence;
+console.log(sentence);*/
+//document.getElementsByClassName("lorem")[0].innerHTML = "sentence";
+
+var x = document.getElementsByClassName("lorem").length;
+//console.log(x); //checking the no. of times lorem class is present
+
+var i, j, k;
+var word = "";
+var sentence = "";
+var h1 = [""];
+var n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//random funtion
+function random(items) {
+	return items[Math.floor(Math.random() * items.length)];
+}
+
+
+//looping no. of times 'lorem' is there
+
+for (k = 0; k < x; k++) {
+	
+	var a = document.getElementsByClassName("lorem")[k].innerHTML;
+	//console.log(a);
+	
+	var ipsum = parseInt(a, 10);
+	//console.log(ipsum);
+	
+	//no. of words
+	for (i = 1; i <= ipsum; i++) {
+
+		//no. of letters
+		var l = random(n);
+
+		for (j = 1; j <= l; j++) {
+			var letter = random(h1);
+			word = word + letter;
+		}
+
+		sentence = sentence + " " + word;
+		word = "";
+	}
+	document.getElementsByClassName("lorem")[k].innerHTML = sentence;
+	sentence = "";
+}
